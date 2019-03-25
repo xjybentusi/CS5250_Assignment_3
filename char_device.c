2b85dc09df6c6ada40ab3275a741d3e4e9e71f43
xjybentusi <xjybentusi@gmail.com>
2019-03-25 23:37:25 +0800

    print error message, everything works

--- char_device.c
+++ char_device.c
@@ -8,8 +8,7 @@
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 #include <linux/uaccess.h>
-
-
+#include <linux/tty.h>
 
 #define MAJOR_NUMBER 61
 
@@ -19,7 +18,6 @@ int onebyte_release(struct inode *inode, struct file *filep);
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
 static void onebyte_exit(void);
-static short  size_of_message;
 
 /* definition of file_operation structure */
 struct file_operations onebyte_fops = {
@@ -40,39 +38,28 @@ int onebyte_release(struct inode *inode, struct file *filep)
      return 0; // always successful
 }
 
- ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
+ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {   
-/*char temp[1];
-char val=(char)(*onebyte_data);
-int wrinten = snprintf(temp, 1, "%s", val);
-	int error_count = copy_to_user(buf, onebyte_data, size_of_message);
-	if (error_count==0){            
-      printk(KERN_INFO "one: Sent %d characters to the user\n", size_of_message);
-      return (size_of_message=0);  // clear the position to the start and return 0
-   }
-   else {
-      printk(KERN_INFO "one: Failed to send %d characters to the user\n", error_count);
-      return -EFAULT;              
-   }*/
 	return simple_read_from_buffer(buf, count, f_pos, onebyte_data, 1);
 }
+
+void print_string(char *string)
+{
+     struct tty_struct *tty;
+     tty = get_current_tty();
+
+     if (tty != NULL) {
+          (tty->driver->ops->write) (tty, string, strlen(string));   
+     }    
+}
+
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-   /*void* temp=NULL;
-	if (sizeof(buf)> sizeof(char)){
-        printk("write error: no space for device");
-        temp=&onebyte_data[0];
-    } else {
-	temp=(onebyte_data);
-	}
-	if (count>sizeof(char)){
-		printk("write error: no space for device!");
-		count=sizeof(char);
-	}*/
-	sprintf(onebyte_data, "%s", buf);   // appending received string with its length
-   size_of_message = strlen(onebyte_data);               // store the length of the stored message
-   printk(KERN_INFO "one: Received %zu characters from the user\n", count);
-   return count;
+     if (count>1){
+	     print_string("bash: printf: write error: No space left on device!");
+     }
+     sprintf(onebyte_data, "%s", buf); 
+     return count;
 }
 
 static int onebyte_init(void)
@@ -96,7 +83,6 @@ static int onebyte_init(void)
      }
      // initialize the value to be X
      *onebyte_data = 'X';
-size_of_message=strlen(onebyte_data);
      printk(KERN_ALERT "This is a onebyte device module\n");
      return 0;
 }
